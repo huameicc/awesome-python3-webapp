b4341864d9a9bd288806bf02eac3b3c46bad2377
huameicc <huameicc@163.com>
2020-04-30 00:11:20 +0800

    RequestHandler func param resolve

--- www/aioweb.py
+++ www/aioweb.py
@@ -47,14 +47,46 @@ def post(path):
     return decorator
 
 
+def _resolve_kwargs(func):
+    _params, _required, _has_var_kwarg = [], [], False
+    for name, param in inspect.signature(func).parameters.items():
+        if param.kind == inspect.Parameter.POSITIONAL_ONLY:
+            raise RuntimeError('POSITIONAL_ONLY param is not supported in a handler.%s %s' % (func.__name__, name))
+        if param.kind == inspect.Parameter.VAR_KEYWORD:
+            _has_var_kwarg = True
+        if param.kind in (inspect.Parameter.POSITIONAL_OR_KEYWORD, inspect.Parameter.KEYWORD_ONLY):
+            _params.append(name)
+            if param.default == inspect.Parameter.empty:
+                _required.append(name)
+    return _params, _required, _has_var_kwarg
+
+
+def _has_request_arg(func):
+    sig = inspect.signature(func)
+    for i, (name, para) in enumerate(sig.items()):
+        if name == 'request' and para.kind in (inspect.Parameter.POSITIONAL_OR_KEYWORD, inspect.Parameter.KEYWORD_ONLY):
+            if i != 0:
+                raise RuntimeError('request must be the first param for a handler if used. %s%s' % (func.__name__, sig))
+            return True
+    return False
+
+
 class RequestHandler:
-    """符合aiohttp的handler标准，即只有一个request"""
+    """符合aiohttp的handler标准，即只有一个request参数"""
     def __init__(self, func):
         self._func = func
+        self._kwargs, self._required_kwargs, self._has_var_kwarg = _resolve_kwargs(func)
+        self._has_request_arg = _has_request_arg(func)
 
     async def __call__(self, request):
-        #todo
-        pass
+        kw = dict()
+        if not self._has_var_kwarg:
+            kwargs = dict()
+            for k in self._kwargs:
+                if k in kw:
+                    kwargs[k] = kw[k]
+            kw = kwargs
+
 
 
 def add_route(app, func):
@@ -72,7 +104,7 @@ def add_route(app, func):
 
 
 def scan_add_routes(app, modpath):
-    """automating add handlers to app by scan <moudle> modpath"""
+    """automatically add handlers to app by scanning <moudle> modpath"""
     r = modpath.rfind('.')
     if r != -1:
         mod = modpath[r+1:]
@@ -97,6 +129,7 @@ def scan_add_routes(app, modpath):
 
 
 if __name__ == '__main__':
+    @template('index.html')
     @get(r'\usr\{id}')
     def f():
         pass
